d9ee08fa4cc93bf7af7c8f58a7d43ed0a652e981
soportecyclops <soportecyclops@gmail.com>
2025-12-15 10:31:37 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,13 @@
+export const AJUSTES = {
+  estado: 0.05,
+  calidad: 0.05,
+  oferta: -0.05,
+  antiguedadAnual: -0.05,
+  maxAjuste: 0.25
+};
+
+export const LIMITES = {
+  comparablesMinimos: 3,
+  outlierMin: 0.7,
+  outlierMax: 1.3
+};
